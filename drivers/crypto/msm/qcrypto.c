3c62821467e5ffeab7e8f962bfafac34bdd724cb
Davide Rombolà <davide.rombola@gmail.com>
2016-01-30 13:47:11 +0100

    defconfig: update hammerhead ported config from 3.4 kernel, something is not compatible for ex: MSM_SMD_PKT in 3.4 is not defined but it's needed in 3.10 for sim card detection more investigations needed

--- drivers/crypto/msm/qcrypto.c
+++ drivers/crypto/msm/qcrypto.c
@@ -1833,6 +1833,9 @@ static int _qcrypto_process_aead(struct  crypto_engine *pengine,
 
 	return ret;
 }
+#ifdef list_next_entry
+#undef list_next_entry
+#endif
 #define list_next_entry(pos, member) \
 		list_entry(pos->member.next, typeof(*pos), member)
 static struct crypto_engine *_qcrypto_static_assign_engine(
